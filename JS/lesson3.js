3d9318f4dcd90a01a2fcd117786d0683ef53920d
ruchibajra <ruchibajracharya8@gmail.com>
2024-05-24 21:35:25 +0545

    loops

---
+++ JS/lesson3.js
@@ -0,0 +1,127 @@
+// // // //LOOPS
+
+// // //for loop
+// // for(let i = 1; i<=10; i++){
+// //     console.log(i);
+// // }
+
+// // //CONTINUE : it skips the given number , BREAK: it terminates after the given number
+// // //EX1:
+// // for(let i = 1; i<=10; i++){
+// //     if(i==5){
+// //         continue;
+// //     }
+// //     console.log(i);
+// // }
+
+// // //EX2:
+// // for(let i = 1; i<=10; i++){
+// //     if(i==5 || i==6 || i==7){
+// //         continue;
+// //     }
+// //     console.log(i);
+   
+// // }
+
+// //WHILE LOOP
+// let i = 1;
+// // while(i <= 10){
+// //     if (i==6){
+// //         i++
+// //         continue;
+// //     }
+// //     console.log(i)
+// //     i++
+// // }
+
+// //DO WHILE LOOP
+// do{
+//  console.log(i);
+//  i++;
+// }while(i<=10);
+
+
+//CLASSWORK
+
+//1: even number from 2 to 10 using while loop
+
+// let i = 2;
+
+// while(i<=10){
+//     console.log(i)
+//     i += 2;
+// }
+
+//2: for loop to calculate the sum of number from 1 to 10
+// let sum = 0;
+
+// for(let i = 1; i<=10; i++){
+//     sum = sum + i;
+// }
+// console.log(sum)
+
+// //3. Using while loop find the factorial of the given number
+
+// let n=5;
+// result = 1;
+
+// while(n>1){
+//     result = result * n;
+//     n--;
+
+// }
+// console.log(result);
+
+
+// //4. Write a for loop that prints the square of numbers from 1 to 5
+
+// for (let i = 1 ; i<=5; i++){
+//     let result = i**2;
+//     console.log(`Square of ${i} is ${result}`)
+// }
+
+// //5. Write a while loop that prints the cube of numbers from 1 to 5
+
+// let i = 1;
+
+// while(i<=5){
+//     let result = i**3;
+//     console.log(`Cube of ${i} is ${result}`)
+//     i++
+// }
+
+// //6. Write a for loop to iterate through an array of names and print each name.
+
+// // Array of names
+// let names = ["Reena", "Ruchi", "Aryan", "Aveshesh"];
+
+// // Iterate through the array using a for loop
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+
+//7. Using while loop, find the sum of the element in an array of numbers 
+// let numbers = [10, 20, 30, 40, 50];
+// let sum = 0;
+// let i = 0;
+
+// while (i < numbers.length){
+//     sum += numbers[i];
+//     i++;
+// }
+// console.log("The sum of the elements in the array is " + sum);
+
+//8. Write a for loop that prints the element in reverse order
+
+
+//9 . Using a while loop, print the first 10 fibonacci numbers.
+
+//10. Write a for loop and print the prime numbers between 1 to 20.
+
+
+
+
+
+
+
+
